a456a6c4261575955f39f6526c775c0c6381e2b4
Star24680 <gulerumaan1@gmail.com>
2024-08-21 18:24:35 +0500

    first commit

---
+++ NotOperator.py
@@ -0,0 +1,18 @@
+A=10
+B=12
+C=12
+print(A!=B)
+print(B!=C)
+A="Python"
+B="Coding"
+if A!=B:
+   print(A,"and",B,"are different")
+A=4
+B=5
+if (A==1)!=(B==5):
+   print("HELLOO")
+X=int(input("ENTER THE NUMBER PLEASE"))
+if X%2!=0:
+   print(X,"is not an even number")
+else:
+   print(X,"is an even number")
