3a6083e85cf53b814ef55675ff16d4ddb20653d8
Star24680 <gulerumaan1@gmail.com>
2024-09-04 17:46:43 +0500

    InitialCommit

---
+++ HELP.py
@@ -0,0 +1,17 @@
+Att=int(input("ENTER ATTENDENCE"))
+
+Y=input("Was student absent for medic condition")
+
+if Att>=75:
+
+    ("STUDENT CAN TAKE EXAM")
+
+else:
+
+    if Y=="yes":
+
+        print("STUDENT CAN TAKE EXAM")
+
+    else:
+        
+        print("STUDENT CANNOT TAKE EXAM")
